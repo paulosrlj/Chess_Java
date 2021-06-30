dc45723f47153b66bca18abb849096167608228d
Paulo <paulo.leite@academico.ifpb.edu.br>
2021-06-30 18:44:47 -0300

    :sparkles: feat - placeNewPiece

--- src/boardGame/Board.java
+++ src/boardGame/Board.java
@@ -1,13 +1,13 @@
 package boardGame;
 
 public class Board {
-    private int rows;
-    private int columns;
+    private final int rows;
+    private final int columns;
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
         if(rows < 1 || columns < 1)
-            throw new BoardException("Error creating board: there must be at least 1 row and 1 column")
+            throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
